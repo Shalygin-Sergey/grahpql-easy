6b63faaf708562e8fe414caaa348eff0fe5a6626
Sergey <bkserega18@yandex.ru>
2023-06-16 09:43:18 +0300

    добавил связь между типами отображаем директоров в запросе фильмов

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,20 @@
 const graphql = require('graphql');
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt } = graphql;
 
 const movies = [
-  { id: '1', name: 'Pulp Fiction', genre: 'Crime' },
-  { id: '2', name: '1984', genre: 'Sci-Fi' },
-  { id: 3, name: 'V for Vendetta', genre: 'Sci-Fi-Triller' },
-  { id: 4, name: 'Snatch', genre: 'Crime-Comedy' },
-]
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1', },
+  { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2', },
+  { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3', },
+  { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4', },
+];
+
+const directors = [
+  { id: '1', name: 'Quentin Tarantino', age: 55 },
+  { id: '2', name: 'Michael Radford', age: 72 },
+  { id: '3', name: 'James McTeigue', age: 51 },
+  { id: '4', name: 'Guy Ritchie', age: 50 },
+];
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
@@ -15,6 +22,21 @@ const MovieType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find(directors => directors.id == parent.id)
+      }
+    }
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -28,6 +50,15 @@ const Query = new GraphQLObjectType({
       },
       resolve(parent, args) {
         return movies.find(movie => movie.id == args.id)
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return directors.find(directors => directors.id == args.id)
       },
     },
   },
